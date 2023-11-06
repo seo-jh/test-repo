3a25a4d66523eb6cb061ff4ee367d470768f21c0
seo-jh <seo688@gmail.com>
2023-11-06 21:15:20 +0900

    print twice

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hello(name_):
     # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
     print(f'Hello, {name_}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+    print(f'Hello, {name_}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
 
 
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
