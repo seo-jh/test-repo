bc6afb27bc54069d37f7ecc65667ea5895a66087
seo-jh <seo688@gmail.com>
2023-11-07 17:37:45 +0900

    main.py remove lines

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ def print_hello(name_):
     # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
     print(f'Hello, {name_}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
     print(f'Hello, {name_}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
-    print(f'Hello, {name_}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
 
 
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
-    print_hello('PyCharm')
     print_hello('PyCharm')
 
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
