a74c4f4c9cc66c40115ca597d21ddc54b49f2917
seo-jh <seo688@gmail.com>
2023-11-08 16:42:02 +0900

    print_hello twice call

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hello(name_):
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
     print_hello('PyCharm')
+    print_hello('PyCharm2')
 
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
